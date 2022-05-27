ab5e715de01428a040aab9856c43374ba945a483
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2022-05-27 20:27:57 +0430

    Add files via upload

--- Sources/install.sh
+++ Sources/install.sh
@@ -3,23 +3,17 @@
 apt upgrade
 apt update
 apt install -y libgmp-dev
-apt install lib32stdc++6
+apt install -y lib32stdc++6
 
-apt install autoconf
-apt install libtool
-apt install g++
-apt install bison
-apt install flex
-apt install texinfo
+apt install -y autoconf
+apt install -y libtool
+apt install -y g++
+apt install -y bison
+apt install -y flex
+apt install -y texinfo
 # apt install libgmp3-dev
-apt install binutils-dev
+apt install -y binutils-dev
 
-wget https://master.dl.sourceforge.net/project/gezel/gezel/2.5/gezel-sources-2.5.deb?viasf
-wget https://master.dl.sourceforge.net/project/gezel/gezel/2.5/gezel-simulavr-2.5.deb?viasf
-wget https://master.dl.sourceforge.net/project/gezel/gezel/2.5/gezel-simitarm-2.5.deb?vias
-wget https://master.dl.sourceforge.net/project/gezel/gezel/2.5/gezel-debug-2.5.deb?viasf
-wget https://master.dl.sourceforge.net/project/gezel/gezel/2.5/gezel-base-2.5.deb?viasf
-wget https://master.dl.sourceforge.net/project/gezel/gezel/2.5/gezel-sources-2.5.deb?viasf
 
 dpkg -i gezel-simitarm-2.5.deb
 dpkg -i gezel-base-2.5.deb
@@ -27,5 +21,3 @@ dpkg -i gezel-simulavr-2.5.deb
 dpkg -i gezel-sources-2.5.deb
 dpkg -i gezel-debug-2.5.deb
 dpkg -i gezel-examples-2.5.deb
-
-export PATH=$:/opt/gezel/bin/
